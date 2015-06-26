f9e59853b3c8caf726386434bcb0d96692928882
Chris Olstrom <chriso@activestate.com>
2015-06-25 17:13:09 -0700

    stackato-ci-scripts/git-webhook/server/server: Simplify code.

--- server/server.rb
+++ server/server.rb
@@ -1,57 +1,39 @@
 require 'sinatra'
 require 'json'
 require 'octokit'
+require 'secure_compare'
 
-ACCESS_TOKEN = ENV['ACCESS_TOKEN']
+ACCESS_TOKEN = ENV.fetch('GITHUB_SECRET').freeze
 
-post '/payload' do
-  # Check the token
-  #client ||= Octokit::Client.new(:access_token => ACCESS_TOKEN)
-  #user = client.user
-  #user.login
-  #puts "Client: #{user}"
-
-  # Get the payload
-  request.body.rewind
-  payload_body = request.body.read
-
-  verify_signature(payload_body, ACCESS_TOKEN)
-  puts "Signature verified!"
-
-  # @payload = JSON.parse(request.body.read)
-  push = JSON.parse(params[:payload])
-  puts "I got some JSON: #{push.inspect}"
-
-  # Process the payload
-  case request.env['HTTP_X_GITHUB_EVENT']
-
-    # Process pull requests
-    when "pull_request"
-      if @payload["action"] == "opened"
-        process_pull_request(@payload["pull_request"])
-      #TODO: process @payload["action"] == "updated"
-      end
-    else process_other_request(@payload)
-  end
+def request_signature
+  request.env.fetch 'HTTP_X_GITHUB_EVENT', nil
 end
 
-# Function: Check the signature of the payload
-def verify_signature(payload_body, secret_token)
-  puts "Payload: #{payload_body}"
-  puts "Token: #{secret_token}"
-
-  hmac_digest = OpenSSL::Digest.new('sha1')
-  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(hmac_digest, secret_token, payload_body)
-  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+def unsigned?
+  request_signature.nil?
 end
 
-helpers do
-  def process_pull_request(pull_request)
-    puts "It's #{pull_request['title']}"
-  end
-
-  def process_other_request(request)
-    puts "It's #{request}"
-  end
+def request_body
+  request.body.rewind.read
 end
 
+def body_signature
+  digest = OpenSSL::Digest.new 'sha1'
+  "sha1=#{OpenSSL::HMAC.hexdigest digest, GITHUB_SECRET, request_body}"
+end
+
+def valid_signature?
+  return false if unsigned?
+  SecureCompare.compare request_signature, body_signature
+end
+
+post '/payload/?' do
+  return 403 unless valid_signature?
+  handler = request.env['HTTP_X_GITHUB_EVENT'].to_sym
+  method(handler).call if methods.include? handler
+end
+
+def pull_request
+  payload = JSON.parse request_body
+  puts payload
+end
