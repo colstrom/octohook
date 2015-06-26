0e0aff1861761ba1de3fd2a4cf07223830023270
Chris Olstrom <chriso@activestate.com>
2015-06-25 21:54:24 -0700

    stackato-ci-scripts/git-webhook/server/lib/git_repository.rb: ... Avoid using {...} for multi-line blocks.

--- server/lib/git_repository.rb
+++ server/lib/git_repository.rb
@@ -10,14 +10,15 @@ module GitRepository
   def changed_files(commit_id)
     return [] unless repository.exists? commit_id
     commit = repository.lookup commit_id
-    commit.parents.first.diff(commit).deltas.map { |delta| delta.new_file[:path] }
+    commit.parents.first.diff(commit).deltas.map do |delta|
+      delta.new_file[:path]
+    end
   end
 
   def changed_components(commit_id)
-    changed_files(commit_id).select {
-      |filename| filename.start_with? 'src'
-    }.map {
-      |filename| filename.split('/').at(1)
-    }.compact.uniq
+    relevant = changed_files(commit_id).select do |filename|
+      filename.start_with? 'src'
+    end
+    relevant.map { |filename| filename.split('/').at(1) }.compact.uniq
   end
 end
