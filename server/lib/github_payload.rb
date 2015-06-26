7f4f49bfb99cf6a08c049ef27607d5a852904e77
Chris Olstrom <chriso@activestate.com>
2015-06-25 22:18:14 -0700

    stackato-ci-scripts/git-webhook/server/lib/github_payload.rb: Add contracts

--- server/lib/github_payload.rb
+++ server/lib/github_payload.rb
@@ -1,21 +1,28 @@
 require 'secure_compare'
+require 'contracts'
 
 GITHUB_SECRET = ENV.fetch('GITHUB_SECRET').freeze
 
 module GitHubPayload
+  include Contracts
+
+  Contract nil => Maybe[String]
   def request_signature
     request.env.fetch 'HTTP_X_GITHUB_EVENT', nil
   end
 
+  Contract nil => String
   def request_body
     request.body.rewind.read
   end
 
+  Contract nil => String
   def body_signature
     digest = OpenSSL::Digest.new 'sha1'
     "sha1=#{OpenSSL::HMAC.hexdigest digest, GITHUB_SECRET, request_body}"
   end
 
+  Contract nil => Bool
   def valid_signature?
     return false unless request_signature
     SecureCompare.compare request_signature, body_signature
