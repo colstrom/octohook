8211c7b5f157f866496a73411e8edd4c1ec7d737
Chris Olstrom <chriso@activestate.com>
2015-06-25 21:13:32 -0700

    stackato-ci-scripts/git-webhook/server/server.rb: Use GitRepository ... Add routes to expose changes made in a commit.

--- server/server.rb
+++ server/server.rb
@@ -3,13 +3,23 @@ require 'json'
 require 'octokit'
 
 require_relative 'lib/github_payload'
+require_relative 'lib/git_repository'
 
 include GitHubPayload
+include GitRepository
 
 post '/payload/?' do
   return 403 unless valid_signature?
   handler = request.env.fetch('HTTP_X_GITHUB_EVENT', 'default').to_sym
   method(handler).call if methods.include? handler
+end
+
+get '/changes/:commit/components/?' do
+  changed_components params[:commit]
+end
+
+get '/changes/:commit/files/?' do
+  changed_files params[:commit]
 end
 
 def pull_request
