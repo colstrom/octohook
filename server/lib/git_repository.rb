b9fbbb89e4b400e37d2ad5b5fc74506c32ac6123
Chris Olstrom <chriso@activestate.com>
2015-06-25 19:26:36 -0700

    stackato-ci-scripts/git-webhook/server/lib/git_repository.rb: ... Add method to get array of changed files.

--- server/lib/git_repository.rb
+++ server/lib/git_repository.rb
@@ -6,4 +6,10 @@ module GitRepository
   def repository
     Rugged::Repository.new REPOSITORY_PATH
   end
+
+  def changed_files(commit_id)
+    return [] unless repository.exists? commit_id
+    commit = repository.lookup commit_id
+    commit.parents.first.diff(commit).deltas.map { |delta| delta.new_file[:path] }
+  end
 end
