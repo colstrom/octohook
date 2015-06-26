bc40d4a1d0514a85dc1249ecbbae247ff12cd67c
Chris Olstrom <chriso@activestate.com>
2015-06-25 19:22:13 -0700

    stackato-ci-scripts/git-webhook/server/server.rb: Use GitHubPayload

--- server/server.rb
+++ server/server.rb
@@ -1,31 +1,10 @@
 require 'sinatra'
 require 'json'
 require 'octokit'
-require 'secure_compare'
 
-GITHUB_SECRET = ENV.fetch('GITHUB_SECRET').freeze
+require_relative 'lib/github_payload'
 
-def request_signature
-  request.env.fetch 'HTTP_X_GITHUB_EVENT', nil
-end
-
-def unsigned?
-  request_signature.nil?
-end
-
-def request_body
-  request.body.rewind.read
-end
-
-def body_signature
-  digest = OpenSSL::Digest.new 'sha1'
-  "sha1=#{OpenSSL::HMAC.hexdigest digest, GITHUB_SECRET, request_body}"
-end
-
-def valid_signature?
-  return false if unsigned?
-  SecureCompare.compare request_signature, body_signature
-end
+include GitHubPayload
 
 post '/payload/?' do
   return 403 unless valid_signature?
