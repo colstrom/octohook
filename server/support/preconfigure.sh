a5fa8cac90bc9e14f40a58c656061fe7d53c8c35
Chris Olstrom <chris@olstrom.com>
2015-07-15 16:26:26 -0700

    support/preconfigure.sh: Clone repository if it doesn't exist, pull otherwise.

--- server/support/preconfigure.sh
+++ server/support/preconfigure.sh
@@ -3,3 +3,13 @@
 cp support/deploy.rsa /home/stackato/.ssh/id_rsa
 chmod 0400 /home/stackato/.ssh/id_rsa
 ssh-keyscan github.com >> /home/stackato/.ssh/known_hosts
+
+REPO="${STACKATO_FILESYSTEM}/upstream"
+
+if [ -d ${REPO} ]; then
+  pushd ${REPO}
+  git pull -f
+  popd
+else
+  git clone git@github.com:ActiveState/stackato-release-st.git ${REPO}
+fi
