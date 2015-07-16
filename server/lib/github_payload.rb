13070dec3dc540fba87f5a370e7e468c42efddd0
Chris Olstrom <chris@olstrom.com>
2015-07-16 06:47:19 -0700

    lib/github_payload.rb: Change input Contracts for methods with no arguments from nil to None.

--- server/lib/github_payload.rb
+++ server/lib/github_payload.rb
@@ -8,24 +8,24 @@ module GitHubPayload
 
   GITHUB_SECRET = (ENV['GITHUB_SECRET'] || CONFIG['github']['secret']).freeze
 
-  Contract nil => Maybe[String]
+  Contract None => Maybe[String]
   def request_signature
     request.env['HTTP_X_HUB_SIGNATURE']
   end
 
-  Contract nil => String
+  Contract None => String
   def request_body
     request.body.rewind
     request.body.read
   end
 
-  Contract nil => String
+  Contract None => String
   def body_signature
     digest = OpenSSL::Digest::SHA1.new
     "sha1=#{OpenSSL::HMAC.hexdigest digest, GITHUB_SECRET, request_body}"
   end
 
-  Contract nil => Bool
+  Contract None => Bool
   def valid_signature?
     return false unless request_signature
     FastSecureCompare.compare request_signature, body_signature
