24ac097145646bf3a9871885694ef407365a81f0
Chris Olstrom <chris@olstrom.com>
2015-07-16 10:29:30 -0700

    support/preconfigure.sh: Only pull. Clone takes so long on the monolithic repo that the health checks fail and the container is terminated before it can finish the job.

--- server/support/preconfigure.sh
+++ server/support/preconfigure.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cp support/deploy.rsa /home/stackato/.ssh/id_rsa
 chmod 0400 /home/stackato/.ssh/id_rsa
 ssh-keyscan github.com >> /home/stackato/.ssh/known_hosts
 
-REPO="${STACKATO_FILESYSTEM}/upstream"
+REPO="${STACKATO_FILESYSTEM}/stackato-release-st"
 
 if [ -d ${REPO} ]; then
   pushd ${REPO}
-  git pull -f
+  git pull
   popd
-else
-  git clone git@github.com:ActiveState/stackato-release-st.git ${REPO}
 fi
