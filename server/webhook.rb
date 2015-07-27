f31caae195d7f041c9448b49588229eae08ad797
Chris Olstrom <chris@olstrom.com>
2015-07-27 11:26:23 -0700

    webhook.rb: Replaced Repository::Local in /changes route Repository::GitHub

--- server/webhook.rb
+++ server/webhook.rb
@@ -18,14 +18,21 @@ post '/payload/?' do
   handler.method(event).call(payload) if handler.methods.include? event
 end
 
+# get '/changes/?:head?/?:base?/?' do
+#   head = params.fetch 'head', Repository::Local.repository.head.target.oid
+#   return 404 unless Repository::Local.repository.exists? head
+
+#   base = params.fetch 'base', Repository::Local.repository.lookup(head).parents.first.oid
+#   return 404 unless Repository::Local.repository.exists? base
+
+#   JSON.dump Repository::Local.changed_components(head, base)
+# end
+
 get '/changes/?:head?/?:base?/?' do
-  head = params.fetch 'head', Repository::Local.repository.head.target.oid
-  return 404 unless Repository::Local.repository.exists? head
-
-  base = params.fetch 'base', Repository::Local.repository.lookup(head).parents.first.oid
-  return 404 unless Repository::Local.repository.exists? base
-
-  JSON.dump Repository::Local.changed_components(head, base)
+  head = params.fetch 'head', Repository::GitHub.head
+  base = params.fetch 'base', Repository::GitHub.parent(head)
+  changed_files = Repository::GitHub.changed_files head, base
+  JSON.dump Repository::Components.changed changed_files
 end
 
 get '/head/?' do
