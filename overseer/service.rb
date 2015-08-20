2f02dd558cb2c9185a78727b27d9e7d62cd93add
Chris Olstrom <chris@olstrom.com>
2015-08-19 18:31:55 -0700

    overseer/service.rb: Add routes for summary and history.

--- overseer/service.rb
+++ overseer/service.rb
@@ -4,7 +4,8 @@ require 'webmachine'
 require_relative 'lib/overseer'
 
 Webmachine.application.routes do
-  add [:*], Overseer::Status
+  add ['history'], Overseer::History
+  add [:*], Overseer::Summary
 end
 
 Thread.new do
