3aafba519d4bf5d128f22ee5218b2a19b05d0098
Chris Olstrom <chris@olstrom.com>
2015-08-19 18:30:59 -0700

    overseer/lib/overseer/history.rb: Add history resource.

---
+++ overseer/lib/overseer/history.rb
@@ -0,0 +1,29 @@
+require 'contracts'
+require 'rakuna'
+
+module Overseer
+  class History < Webmachine::Resource
+    include Contracts
+    include Rakuna::Storage::Redis
+    include Rakuna::Content::JSON
+    include Overseer::Support::Pipeline
+
+    Contract None => Hash
+    def output
+      pipeline.each_with_object({}) do |queue, h|
+        h[queue] = history(queue).sort { |a, b| a['id'] <=> b['id'] }
+      end
+    end
+
+    private
+
+    Contract String => ArrayOf[Hash]
+    def history(queue)
+      redis.keys("#{queue}:task:*")
+        .select { |task| task.match /\d$/ }
+        .map do |task|
+          redis.hgetall(task).merge({ 'id' => task.split(':').last.to_i })
+        end
+    end
+  end
+end
