adea3ac118351de86bcc4a15ed96c786ef8ca39e
Chris Olstrom <chris@olstrom.com>
2015-09-15 09:50:44 -0700

    reporter: Rescue exception, mark job unworkable.

--- reporter/service.rb
+++ reporter/service.rb
@@ -13,6 +13,10 @@ reporting = Kanban::Backlog.new backend: Redis.new, namespace: 'reporting'
 loop do
   id = reporting.claim
   spec = reporting.get id
-  GitHub.update_status spec, Jenkins::Translate.to_github(spec['status'])
-  reporting.complete id
+  begin
+    GitHub.update_status spec, Jenkins::Translate.to_github(spec['status'])
+    reporting.complete id
+  rescue Octokit::UnprocessableEntity
+    reporting.unworkable id
+  end
 end
